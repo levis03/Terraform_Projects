798f31af945114e88bd6a3625fddd36519d5ca4e
Levis Maina <88320351+levis03@users.noreply.github.com>
2022-08-19 11:37:12 +0300

    Added VPC and Subnet

--- single_server.tf
+++ single_server.tf
@@ -3,8 +3,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#Creating a VPC
+resource "aws_vpc" "first-vpc" {
+ cidr_block = "10.0.0.0/16" 
+ tags = {
+   "Name" = "production"
+ }
+}
+#Adding a subnet to the VPC
+resource "aws_subnet" "subnet-1" {
+  vpc_id =aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "first-Subnet"
+  }
+}
+
 #Configuring the resource
-resource "aws_instance" "example" {
+resource "aws_instance" "firstServer" {
   ami = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
 
